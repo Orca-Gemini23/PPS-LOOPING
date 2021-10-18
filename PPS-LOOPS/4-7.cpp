da3770b986436603b167c945a6806641583dbc91
orca_gemini <cs.yash95@gmail.com>
2021-10-18 16:07:52 +0530

    second commit

--- PPS-LOOPS/4-7.cpp
+++ PPS-LOOPS/4-7.cpp
@@ -3,12 +3,19 @@
 #include <math.h>
 using namespace std;
 int main(){
-    int lim, num, i;
-    cout << "Enter the number limit: ";
-    cin >> lim;
-    for (i = 1; i <= lim; i++){
-        cout << "Enter the numbers:";
+    int num, i, sum = 0;
+    do{
+        cout << "Enter the numbers: ";
         cin >> num;
-    }
+        if(num % 3 == 0){
+            cout << "Critera unsatisfied" << endl;
+        }
+        else{
+            sum = sum + num;
+            cout << sum << endl;
+            i = i + 1;
+        }i = i + 1;
+        
+    }while(num > 0);
     return 0;
 }
